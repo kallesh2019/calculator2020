66767e86b43f3a3f456d304f53f4313b124011bf
Kallesh <kallesh9900@gmail.com>
2020-05-11 17:30:02 +0000

    src code edited

--- src/test/java/com/example/calculator/CalculatorTest.java
+++ src/test/java/com/example/calculator/CalculatorTest.java
@@ -13,17 +13,3 @@ public class CalculatorTest {
      }
 }
 
-package com.example.calculator;
-
-
-import org.junit.Test;
-import static org.junit.Assert.assertEquals;
-
-public class CalculatorTest {
-     private Calculator calculator = new Calculator();
-
-     @Test
-     public void testSum() {
-          assertEquals(5, calculator.sum(2, 3));
-     }
-}
